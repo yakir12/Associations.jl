bc336107077d2a44872abc8ef54ee333e51b6d6e
Yakir Luc Gagnon <12.yakir@gmail.com>
2017-04-08 21:26:01 +0200

    depend

--- deps/build.jl
+++ deps/build.jl
@@ -1,5 +1,4 @@
 using BinDeps
-using Compat
 
 @BinDeps.setup
 
@@ -10,13 +9,13 @@ provides(AptGet, "mediainfo", mediainfo)
 provides(Yum, "mediainfo", mediainfo)
 provides(Pacman, "mediainfo", mediainfo)
 
-if is_apple() 
     provides(Sources, URI("https://mediaarea.net/download/binary/mediainfo/0.7.94/MediaInfo_CLI_0.7.94_Mac.dmg"), mediainfo)
-end
+#if is_apple() 
+#end
 
-if is_windows() 
     provides(Sources, URI("https://mediaarea.net/download/binary/mediainfo/0.7.94/MediaInfo_CLI_0.7.94_Windows_x64.zip"), mediainfo)
-end
+#if is_windows() 
+#end
 
 provides(BuildProcess, Autotools(libtarget = "mediainfo"), mediainfo)
 
