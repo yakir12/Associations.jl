794da93bb3a7f3779f1552c1b27b14e7f84499cb
Yakir Luc Gagnon <12.yakir@gmail.com>
2017-04-08 21:24:06 +0200

    depend

--- deps/build.jl
+++ deps/build.jl
@@ -12,12 +12,12 @@ provides(Pacman, "mediainfo", mediainfo)
 
 if is_apple() 
     provides(Sources, URI("https://mediaarea.net/download/binary/mediainfo/0.7.94/MediaInfo_CLI_0.7.94_Mac.dmg"), mediainfo)
-    provides(BuildProcess, Autotools(libtarget = "mediainfo"), mediainfo)
 end
 
 if is_windows() 
     provides(Sources, URI("https://mediaarea.net/download/binary/mediainfo/0.7.94/MediaInfo_CLI_0.7.94_Windows_x64.zip"), mediainfo)
-    provides(BuildProcess, Autotools(libtarget = "mediainfo"), mediainfo)
 end
+
+provides(BuildProcess, Autotools(libtarget = "mediainfo"), mediainfo)
 
 @BinDeps.install Dict(:mediainfo => :mediainfo)
