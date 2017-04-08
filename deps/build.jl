8c2c87ce92596733653c795469954cf0b3f4a5f8
Yakir Luc Gagnon <12.yakir@gmail.com>
2017-04-08 21:32:27 +0200

    depend

--- deps/build.jl
+++ deps/build.jl
@@ -2,21 +2,21 @@ using BinDeps
 
 @BinDeps.setup
 
-mediainfo = library_dependency("mediainfo")
+mediainfo = library_dependency("mediainfo", aliases = ["mediainfo.exe"])
 
 # package managers
 provides(AptGet, "mediainfo", mediainfo)
 provides(Yum, "mediainfo", mediainfo)
 provides(Pacman, "mediainfo", mediainfo)
 
-    provides(Sources, URI("https://mediaarea.net/download/binary/mediainfo/0.7.94/MediaInfo_CLI_0.7.94_Mac.dmg"), mediainfo)
+provides(Binaries, URI("https://mediaarea.net/download/binary/mediainfo/0.7.94/MediaInfo_CLI_0.7.94_Mac.dmg"), mediainfo, os=:Mac)
 #if is_apple() 
 #end
 
-    provides(Sources, URI("https://mediaarea.net/download/binary/mediainfo/0.7.94/MediaInfo_CLI_0.7.94_Windows_x64.zip"), mediainfo)
+provides(Binaries, URI("https://mediaarea.net/download/binary/mediainfo/0.7.94/MediaInfo_CLI_0.7.94_Windows_x64.zip"), mediainfo, os=:Windows)
 #if is_windows() 
 #end
 
 provides(BuildProcess, Autotools(libtarget = "mediainfo"), mediainfo)
 
-@BinDeps.install Dict(:mediainfo => :mediainfo)
+@BinDeps.install #Dict(:mediainfo => :mediainfo)
