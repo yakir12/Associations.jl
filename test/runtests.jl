c65b80f4e92960947928ef0f361e250b31f5da83
Yakir Luc Gagnon <12.yakir@gmail.com>
2017-04-10 14:44:23 +0200

    tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,12 @@ using Base.Test
 folder = joinpath(Pkg.dir("Associations"), "test", "videofolder")
 files = Associations.getVideoFiles(folder)
 vf = [Associations.VideoFile(folder, file) for file in files]
+a = [Associations.VideoFile("a.mp4",[DateTime("2017-02-28T16:04:47")]), Associations.VideoFile("b.mp4",[DateTime("2017-03-02T15:38:25")])]
+files = ["a.mp4", "b.mp4"]
 # write your own tests here
-@test length(vf) == 1
-@test vf[1] == Associations.VideoFile("a.mp4",[DateTime("2017-02-28T16:04:47")])
+@test length(vf) == 2
+for file in files
+    ai = filter(x -> x.file == file, a)
+    v = filter(x -> x.file == file, vf)
+    @test ai == v
+end
