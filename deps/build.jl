e930fb0ac54c420b0182d3dc46d3f5edbb0b1c9d
Yakir Luc Gagnon <12.yakir@gmail.com>
2017-04-10 13:20:59 +0200

    build

--- deps/build.jl
+++ deps/build.jl
@@ -3,9 +3,12 @@ using BinDeps
 @BinDeps.setup
 
 exiftool = library_dependency("exiftool", aliases=["exiftool.exe"])
+if is_windows()
+    provides(Sources, URI("http://www.sno.phy.queensu.ca/~phil/exiftool/exiftool-10.48.zip"), exiftool, os = :Windows, unpacked_dir = "downloads")
+end
 
 provides(Sources, URI("http://www.sno.phy.queensu.ca/~phil/exiftool/Image-ExifTool-10.48.tar.gz"), exiftool, unpacked_dir = "downloads")
-provides(BuildProcess, Autotools(libtarget = "exiftool.la"), exiftool)
+provides(BuildProcess, Autotools(libtarget = "exiftool"), exiftool)
 
 try
     @BinDeps.install Dict(:exiftool => :exiftool)
