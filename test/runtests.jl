ea53e6e3f8d0159320c7f8ad41f8756f211e333c
Yakir Luc Gagnon <12.yakir@gmail.com>
2017-04-10 13:04:20 +0200

    tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,4 +6,4 @@ files = Associations.getVideoFiles(folder)
 vf = [Associations.VideoFile(folder, file) for file in files]
 # write your own tests here
 @test length(vf) == 1
-@test vf[1] == Associations.VideoFile("a.MTS",[DateTime("2016-11-11T22:32:14")])
+@test vf[1] == Associations.VideoFile("a.mp4",[DateTime("2017-02-28T16:04:47")])
