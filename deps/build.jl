7cb2b2d7d048ac7eebe773132ab3fe9b36b33942
Yakir Luc Gagnon <12.yakir@gmail.com>
2017-04-10 13:23:52 +0200

    build

--- deps/build.jl
+++ deps/build.jl
@@ -4,10 +4,13 @@ using BinDeps
 
 exiftool = library_dependency("exiftool", aliases=["exiftool.exe"])
 if is_windows()
-    provides(Sources, URI("http://www.sno.phy.queensu.ca/~phil/exiftool/exiftool-10.48.zip"), exiftool, os = :Windows, unpacked_dir = "downloads")
+    provides(Sources, URI("http://www.sno.phy.queensu.ca/~phil/exiftool/exiftool-10.48.zip"), exiftool, os = :Windows)
 end
 
-provides(Sources, URI("http://www.sno.phy.queensu.ca/~phil/exiftool/Image-ExifTool-10.48.tar.gz"), exiftool, unpacked_dir = "downloads")
+if is_unix()
+    provides(Sources, URI("http://www.sno.phy.queensu.ca/~phil/exiftool/Image-ExifTool-10.48.tar.gz"), exiftool)
+end
+
 provides(BuildProcess, Autotools(libtarget = "exiftool"), exiftool)
 
 try
