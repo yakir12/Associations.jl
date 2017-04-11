0248d4d8eeba0d6cdb3687906983c9356b2b4f14
Yakir Luc Gagnon <12.yakir@gmail.com>
2017-04-11 09:10:33 +0200

    separated into gui.jl and util.jl

---
+++ src/util.jl
@@ -0,0 +1,32 @@
+function shorten(s::String, k::Int)::String
+    m = length(s)
+    m > 2k || return s
+    s[1:k]*"…"*s[end-k + 1:end]
+end
+function shorten(vfs::Vector{String})
+    for k = 20:max(20, maximum(length.(vfs)))
+        shortnames = Dict{String, String}()
+        tooshort = false
+        for vf in vfs
+            key = shorten(vf, k)
+            if haskey(shortnames, k)
+                tooshort = true
+                break
+            end
+            shortnames[key] = vf
+        end
+        tooshort || return shortnames
+    end
+end
+function openit(f::String)
+    if is_windows()
+        run(`start $f`)
+    elseif is_linux()
+        run(`xdg-open $f`)
+    elseif is_apple()
+        run(`open $f`)
+    else
+        error("Couldn't open $f")
+    end
+end
+
