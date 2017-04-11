3633236fd8a53cc9ef310ac62419704eee47b6a7
Yakir Luc Gagnon <12.yakir@gmail.com>
2017-04-11 09:13:16 +0200

    build

--- deps/build.jl
+++ deps/build.jl
@@ -3,7 +3,8 @@ using BinDeps
 @BinDeps.setup
 
 exiftool = library_dependency("exiftool", aliases=["exiftool.exe"])
-if is_windows()
+
+if is_windows() # doesn't seem to work on Jochen's laptop. But works when done manually
     provides(Sources, URI("http://www.sno.phy.queensu.ca/~phil/exiftool/exiftool-10.48.zip"), exiftool, os = :Windows)
 end
 
@@ -13,7 +14,7 @@ end
 
 provides(BuildProcess, Autotools(libtarget = "exiftool"), exiftool)
 
-try
+try # this works but produvces an error
     @BinDeps.install Dict(:exiftool => :exiftool)
 end
 
