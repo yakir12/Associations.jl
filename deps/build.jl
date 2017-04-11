f4e17a2864064b0f7cd133a498109b51f924f7f0
Yakir Luc Gagnon <12.yakir@gmail.com>
2017-04-11 23:33:20 +0200

    build

--- deps/build.jl
+++ deps/build.jl
@@ -1,6 +1,6 @@
 using BinDeps
 
-@BinDeps.setup
+#=@BinDeps.setup
 
 exiftool = library_dependency("exiftool", aliases=["exiftool.exe"])
 
@@ -16,5 +16,46 @@ provides(BuildProcess, Autotools(libtarget = "exiftool"), exiftool)
 
 try # this works but produvces an error
     @BinDeps.install Dict(:exiftool => :exiftool)
-end
+end=#
 
+basedir = dirname(@__FILE__)
+
+program = "exiftool"
+#prefix = joinpath(basedir, "usr")
+if is_unix()
+    file = "Image-ExifTool-10.49"
+    extension = ".tar.gz"
+    binary_name = program
+end
+if is_windows()
+    file = "exiftool-10.49"
+    extension = ".zip"
+    binary_name = "program.exe"
+end
+filename = file*extension
+url = "http://www.sno.phy.queensu.ca/~phil/exiftool/$filename"
+
+run(@build_steps begin
+              FileDownloader(url, joinpath(basedir, "downloads", filename))
+              CreateDirectory(joinpath(basedir, "src"))
+              FileUnpacker(joinpath(basedir, "downloads", filename), joinpath(basedir, "src"), "")
+end)
+
+f = first(sort(filter(r"exiftool", readdir(joinpath(basedir, "src", file))), by = length))
+
+
+
+
+
+#=basedir = joinpath(Pkg.dir("Associations"), "deps")
+downloadsdir = joinpath(basedir, "downloads")
+srcdir = joinpath(basedir, "src")
+
+run(@build_steps begin
+    CreateDirectory(downloadsdir)
+    FileDownloader(url, joinpath(downloadsdir, filename))
+    CreateDirectory(joinpath(basedir, "src"))
+    FileUnpacker(downloads,
+                 joinpath(basedir, "src"),
+                 "")
+    end)=#
