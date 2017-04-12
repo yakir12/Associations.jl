764cc41caefd2668b5e6d1a094002c547981a8c5
Yakir Luc Gagnon <12.yakir@gmail.com>
2017-04-12 13:31:48 +0200

    add launcher

--- src/main.jl
+++ src/main.jl
@@ -1,3 +1,4 @@
+#!/usr/bin/julia
 using Gtk, Associations
 folder = Gtk.open_dialog("Select videos-folder", action=Gtk.GtkFileChooserAction.SELECT_FOLDER)
 #folder = open_dialog("Select a file in the videos-folder")
