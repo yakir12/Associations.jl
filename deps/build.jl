e49907d46adcd8f41dd784031520b81811e9b08e
Yakir Luc Gagnon <12.yakir@gmail.com>
2017-04-12 11:06:47 +0200

    build

--- deps/build.jl
+++ deps/build.jl
@@ -20,6 +20,7 @@ end
 filename = file*extension
 url = "http://www.sno.phy.queensu.ca/~phil/exiftool/$filename"
 
+if is_unix()
 run(
     @build_steps begin
     FileDownloader(url, joinpath(basedir, "downloads", filename))
@@ -27,12 +28,18 @@ run(
     FileUnpacker(joinpath(basedir, "downloads", filename), joinpath(basedir, "src"), "")
     end
    )
-
-if is_unix()
     mv(joinpath(basedir, "src", file), joinpath(basedir, "src", "exiftool"), remove_destination = true)
 end
 
 if is_windows()
+run(
+    @build_steps begin
+    FileDownloader(url, joinpath(basedir, "downloads", filename))
+    CreateDirectory(joinpath(basedir, "src"))
+    FileUnpacker(joinpath(basedir, "downloads", filename), joinpath(basedir, "src"), target)
+    end
+   )
+
     mv(joinpath(basedir, "src", target), joinpath(basedir, "src", "exiftool", binary_name))
     symlink(joinpath(basedir, "src", "exiftool",binary_name), joinpath(basedir, "src", "exiftool", program))
 end
